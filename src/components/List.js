cd3bae32005327eb4452223828c98f2b41c5bd69
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 17:47:16 -0400

    Add ListContainer styled-component

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 const List = (props) => {
 	return (
-		<>
+		<ListContainer>
 			<h2>{props.title}</h2>
 			<ul>
 				{props.items.map(item => <li>{item}</li>)}
 			</ul>
-		</>
+		</ListContainer>
 	)
 }
 
 export default List;
+
+const ListContainer = styled.div`
+	h2 {
+		font-family: 'Exo', sans-serif;
+	}
+	li {
+		font-family: 'Muli', sans-serif;
+	}
+`;
