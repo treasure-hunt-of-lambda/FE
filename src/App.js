6493356ac2769cbd52086d265f26d05f26657f4e
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 16:50:18 -0400

    Remove anoying scroll bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import panzoom from "panzoom";
 
 import Map from "./components/Map";
+import SideBar from "./components/SideBar";
 
 import mapdata from './components/mapdata';
 
@@ -12,24 +13,31 @@ function App() {
     const scrollable = document.getElementById('scrollable');
     const panzoomInstance = panzoom(scrollable, {
         maxZoom: 5,
-        minZoom: 0.1
+        minZoom: 1,
+        bounds: true
+
       })
     panzoomInstance.zoomAbs(
         0, // initial x position
-        5000, // initial y position
+        0, // initial y position
         1  // initial zoom 
       )
-    panzoomInstance.moveTo(0,-50)
+    panzoomInstance.moveTo(0,0)
   },[])
 
   return (
     <>
-      <div id = "scrollable">
-        <Map data = {mapdata} height={1000} width={1000} />  
-      </div>
-
+      <Scrollable>
+        <Map data = {mapdata} height={1000} width={1000} id = "scrollable"  />  
+      </Scrollable>
+      <SideBar/>
     </>
   );
 }
 
 export default App;
+
+const Scrollable = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
