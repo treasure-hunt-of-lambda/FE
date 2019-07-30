6f7810e3adac522db3958c86a92c061f35125274
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 18:31:23 -0400

    Create Controls.js

---
+++ src/components/Controls.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Controls = (props) => {
+	return (
+		<ControlsWrapper>
+
+		</ControlsWrapper>	
+	)
+}
+
+export default Controls;
+
+const ControlsWrapper = styled.div`
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	height: 50px;
+	width: 75vw;
+	background: blue;
+`;
