452f4882bb1528f5acf97e726ddbf70f93f69d7c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 15:10:04 -0400

    Render map with test data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import Map from "./components/Map";
+
+import mapdata from './components/mapdata';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Scrollable >
+      <h1>Map should render here</h1>
+      <Map data = {mapdata} height={1000} width={1000}/>  
+    </Scrollable>
   );
 }
 
 export default App;
+
+const Scrollable = styled.div`
+  height: 500px;
+  width: 500px;
+  overflow: scroll;
+`;
