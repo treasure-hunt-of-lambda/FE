d0c5499b4c5096d9a2b5115a1e88a2f4f21bd0e2
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 16:04:24 -0400

    Make map zoomable and pannable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from "styled-components";
+import panzoom from "panzoom";
 
 import Map from "./components/Map";
 
 import mapdata from './components/mapdata';
 
 function App() {
+
+  useEffect(() => {
+    const scrollable = document.getElementById('scrollable');
+    const panzoomInstance = panzoom(scrollable, {
+        maxZoom: 5,
+        minZoom: 0.1
+      })
+    panzoomInstance.zoomAbs(
+        0, // initial x position
+        5000, // initial y position
+        1  // initial zoom 
+      )
+    panzoomInstance.moveTo(0,-50)
+  },[])
+
   return (
-    <Scrollable >
-      <h1>Map should render here</h1>
-      <Map data = {mapdata} height={1000} width={1000}/>  
-    </Scrollable>
+    <>
+      <div id = "scrollable">
+        <Map data = {mapdata} height={1000} width={1000} />  
+      </div>
+
+    </>
   );
 }
 
 export default App;
-
-const Scrollable = styled.div`
-  height: 500px;
-  width: 500px;
-  overflow: scroll;
-`;
