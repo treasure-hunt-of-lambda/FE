39f82ef8a9e4e7b3a85d88c491f19a47468435c0
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 16:50:52 -0400

    Create SideBar.js

---
+++ src/components/SideBar.js
@@ -0,0 +1,21 @@
+import React, {  } from "react";
+import styled from "styled-components";
+
+const SideBar = (props) => {
+	return (
+		<SidebarWrapper>
+
+		</SidebarWrapper>
+	)
+}
+
+export default SideBar;
+
+const SidebarWrapper = styled.div`
+	position: fixed;
+	top: 0;
+	right: 0;
+	height: 100vh;
+	width: 500px;
+	background: blue;
+`;
