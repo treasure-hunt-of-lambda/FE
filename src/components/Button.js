14e7a121464bb7c22bbea693668a3b2e85d1ba8c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 20:46:28 -0400

    Add Button Style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,54 @@
 import React from "react";
 import styled, {css} from "styled-components";
 
-const Button = (Icon) => ({color, click}) => {
+const Button = ({color, hoverColor, click, Icon}) => {
 	return (
-		<ButtonWrapper color = {color}>
-			<Icon onClick = {click}/>
+		<ButtonWrapper color = {color} hoverColor = {hoverColor} onClick = {click}>
+			<Icon/>
 		</ButtonWrapper>
 	)
 }
 
 export default Button;
 
-const ButtonWrapper = styled.div`
-	${props => props.color && css`
+const ButtonWrapper = styled.button`
+	// referenced 
+	// https://css-tricks.com/overriding-default-button-styles/
+
+	display: inline-block;
+	border: none;
+	padding: 1rem 2rem;
+	margin: 0;
+	text-decoration: none;
+	background: #0069ed;
+	color: #ffffff;
+	font-family: sans-serif;
+	font-size: 1rem;
+	cursor: pointer;
+	text-align: center;
+	transition: background 250ms ease-in-out, 
+				transform 150ms ease;
+	-webkit-appearance: none;
+	-moz-appearance: none;
+
+	&:hover,
+	&:focus {
+		background: #0053ba;
+	}
+
+	&:focus {
+		outline: 1px solid #fff;
+		outline-offset: -4px;
+	}
+
+	&:active {
+		transform: scale(0.99);
+	}
+
+	${props => props.color && props.hoverColor && css`
 		background-color: ${props.color}
+		&:hover {
+			background-color: ${props.hoverColor}
+		}
 	`}
 `;
