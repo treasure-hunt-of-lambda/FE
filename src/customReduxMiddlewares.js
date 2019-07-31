759d8b974e9b6c7b16b3ee497d6a591c05c1ce28
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 17:14:33 -0400

    Rename checkTimeOut to setLastAction

--- src/customReduxMiddlewares.js
+++ src/customReduxMiddlewares.js
@@ -1,14 +1,9 @@
 import * as actions from "./actions";
 
-export const checkTimeOut = state => next => action => {
-	// console.log(state.getState().explore.lastAction, state.getState().explore.cooldown, (new Date() - state.getState().explore.lastAction)/1000)
-	if (action.type !== actions.INIT) {
-		const currentState = state.getState()
-		const elapsed = new Date() - currentState.explore.lastAction;
-		if (elapsed < currentState.cooldown){
-			alert(`Can't make action yet. Please wait ${currentState.cooldown - elapsed} `)
-			next(action)
-		}
+export const setLastAction = state => next => action => {
+	const exploreActions = [actions.INIT, actions.MOVE];
+	if (exploreActions.includes(action.type)) {
+		action.payload.lastAction = Date.now();
 	}
 	next(action)
 }
