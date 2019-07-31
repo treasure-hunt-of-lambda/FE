9998c4ae1894a0ddb289a2b6d87f8c0ce74d2662
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-30 20:46:54 -0400

    Add Buttons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import {ToggleLeft, ToggleRight, Circle} from "react-feather";
+
+import Button from "./Button";
 
 const Controls = (props) => {
 	return (
 		<ControlsWrapper>
-
+			<Button color = "#778DA9" hoverColor = "#6D819A" click = {()=> console.log("click")} Icon = {ToggleLeft}/>
+			<Button color = "#5D75AD" hoverColor = "#556B9E" click = {()=> console.log("click")} Icon = {Circle}/>
+			<Button color = "#479B71" hoverColor = "#57A47D" click = {()=> console.log("click")} Icon = {Circle}/>
+			<Button color = "#825167" hoverColor = "#8D6074" click = {()=> console.log("click")} Icon = {Circle}/>
+			<Button color = "#E0D64C" hoverColor = "#E2D95C" click = {()=> console.log("click")} Icon = {Circle}/>
 		</ControlsWrapper>	
 	)
 }
@@ -17,5 +24,7 @@ const ControlsWrapper = styled.div`
 	left: 0;
 	height: 50px;
 	width: 75vw;
-	background: blue;
+	background: #2F394C;
+
+	display: flex;
 `;
