f860d4dbb7eae5d47f0f9c157bd9c1a6035e5f90
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 17:36:32 -0400

    Pass down relevant data to Buttons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -12,18 +12,19 @@ const Controls = (props) => {
 	return (
 		<ControlsWrapper>
 			<Button color = "#778DA9" hoverColor = "#6D819A" click = {()=> console.log("click")} Icon = {ToggleLeft}/>
-			<Button color = "#5D75AD" hoverColor = "#556B9E" click = {()=> props.move("n", props.lastAction, props.cooldown)} Icon = {Circle}/>
-			<Button color = "#479B71" hoverColor = "#57A47D" click = {()=> props.move("s", props.lastAction, props.cooldown)} Icon = {Circle}/>
-			<Button color = "#825167" hoverColor = "#8D6074" click = {()=> props.move("e", props.lastAction, props.cooldown)} Icon = {Circle}/>
-			<Button color = "#E0D64C" hoverColor = "#CCC346" click = {()=> props.move("w", props.lastAction, props.cooldown)} Icon = {Circle}/>
+			<Button color = "#5D75AD" hoverColor = "#556B9E" click = {()=> props.move(props.room_id, "n", props.lastAction, props.cooldown)} Icon = {Circle}/>
+			<Button color = "#479B71" hoverColor = "#57A47D" click = {()=> props.move(props.room_id, "s", props.lastAction, props.cooldown)} Icon = {Circle}/>
+			<Button color = "#825167" hoverColor = "#8D6074" click = {()=> props.move(props.room_id, "e", props.lastAction, props.cooldown)} Icon = {Circle}/>
+			<Button color = "#E0D64C" hoverColor = "#CCC346" click = {()=> props.move(props.room_id, "w", props.lastAction, props.cooldown)} Icon = {Circle}/>
 		</ControlsWrapper>	
 	)
 }
 
 const mstp = state => {
 	return {
+		room_id: state.explore.room_id,
 		lastAction: state.explore.lastAction,
-		cooldown: state.explore.cooldown
+		cooldown: state.explore.cooldown,
 	}
 }
 
