60b68623c854bcadb2bd56048b9aea1372b2d6d3
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 19:02:43 -0400

    Handle conditional render and add styles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,55 @@
 import React from "react";
+import styled, {css} from "styled-components";
+import {Download} from "react-feather";
+
 
 const ListItem = (props) => {
-
+	return (
+		<StyledItem>
+		{props.actionable ? <ItemButton><Download/></ItemButton> : null}
+		<p>{props.item}</p>
+		</StyledItem>
+	)
 }
 
 export default ListItem;
+
+const StyledItem = styled.li`
+	word-break: break-word;
+	display: flex;
+    align-items: center;
+`;
+
+const ItemButton = styled.button`
+	display: inline-block;
+	border: none;
+	padding: 10px;
+	margin: 0 5px;
+	text-decoration: none;
+	background: none;
+	color: #ffffff;
+	font-family: sans-serif;
+	font-size: 1rem;
+	cursor: pointer;
+	text-align: center;
+	transition: background 250ms ease-in-out, 
+				transform 150ms ease;
+	-webkit-appearance: none;
+	-moz-appearance: none;
+
+	padding-top: 13px;
+    padding-bottom: 13px;
+
+	outline: none;
+	&:active {
+		transform: scale(0.99);
+	}
+
+	${props => props.color && props.hoverColor && css`
+		background-color: ${props.color}
+		&:hover {
+			background-color: ${props.hoverColor}
+		}
+
+	`}
+`;
