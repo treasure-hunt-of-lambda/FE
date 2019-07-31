0a570f9607be5ce525ee57ebb0c29274d6001edb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 14:35:55 -0400

    Add move() and remove unused code

--- src/actions/exploreActions.js
+++ src/actions/exploreActions.js
@@ -3,6 +3,8 @@ const baseURL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/";
 const authToken = `Token ${process.env.REACT_APP_PLAYER_KEY}`;
 
 export const INIT = "INIT";
+export const MOVE = "MOVE";
+
 const axiosWithAuth = axios.create({
 	baseURL: baseURL,
 	timeout: 1000,
@@ -11,12 +13,8 @@ const axiosWithAuth = axios.create({
 	}
 });
 
-export const init = (data) => dispatch => {
-	axiosWithAuth.get("/init", {
-		Headers: {
-			Authorization: authToken
-		}
-	}).then(res => {
+export const init = () => dispatch => {
+	axiosWithAuth.get("/init").then(res => {
 		dispatch({
 			type: INIT,
 			payload: res.data
@@ -24,4 +22,18 @@ export const init = (data) => dispatch => {
 	}).catch(err => {
 		console.log(err)
 	})
+}
+
+export const move = (dir) => dispatch => {
+	axiosWithAuth("/move", {
+		direction: dir
+	}).then(res => {
+		dispatch({
+			type: MOVE,
+			payload: res.data
+		})
+	}).catch(err => {
+		console.log(err)
+	})
+
 }
