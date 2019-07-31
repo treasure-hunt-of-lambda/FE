8c6ec2ff5caa2bc5a148c0eba16ab1752b3fd1b0
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 13:54:55 -0400

    Handle INIT action

---
+++ src/reducers/exploreReducer.js
@@ -0,0 +1,24 @@
+import * as actions from "../actions";
+
+const initialState = {
+	"room_id": "id",
+	"title": "Room",
+	"description": "Description",
+	"coordinates": "(0,0)",
+	"elevation": 0,
+	"terrain": "NORMAL",
+	"players": [],
+	"items": [],
+	"exits": [],
+	"cooldown": 1.0,
+	"errors": [],
+	"messages": []
+}
+
+const branchTable = {
+	[actions.INIT]: (state, action) => {return {...action.payload}}
+}
+
+export default (state = initialState, action) => {
+	return action.type in branchTable ? branchTable[action.type](state, action) : state;
+}
