386788bf4b6933baa65da476220320a53fc4a513
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-08-01 15:21:38 -0400

    Move travel() to helper_funcs

--- traversalCode/helper_funcs.py
+++ traversalCode/helper_funcs.py
@@ -1,6 +1,12 @@
 from queue import Queue
 
+import requests
+import time
+
 # from map import data
+
+base_url = "https://lambda-treasure-hunt.herokuapp.com/api/adv"
+headers = {"Authorization": "Token 3b5ce4bde563d93d6ab89e3b8b9afd874e87f196"}
 
 def getPathToRoom(map, starting_room, target_room):
 	visited = []
@@ -18,3 +24,11 @@ def getPathToRoom(map, starting_room, target_room):
 				new_path = new_room_path[::]
 				new_path.append((f'{exit}', exit_dir))
 				q.put(new_path)
+
+# return travel data
+def travel(map, current_room, dir):
+	r = requests.post(f"{base_url}/move/", headers = headers, json= {"direction": dir, "next_room_id": f"{map[current_room][dir]}"}) 
+	print(r)
+	data = r.json()
+	time.sleep(data["cooldown"])
+	return  data
