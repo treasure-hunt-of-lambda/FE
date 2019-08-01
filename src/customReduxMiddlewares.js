8e63f8acbcfd1af35cdd66de705659078410f454
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 23:08:50 -0400

    Update List

--- src/customReduxMiddlewares.js
+++ src/customReduxMiddlewares.js
@@ -1,7 +1,7 @@
 import * as actions from "./actions";
 
 export const setLastAction = state => next => action => {
-	const exploreActions = [actions.INIT, actions.MOVE];
+	const exploreActions = [actions.INIT, actions.MOVE, actions.REFRESH_INVENTORY_STATUS, actions.DROP_ITEM];
 	if (exploreActions.includes(action.type)) {
 		action.payload.lastAction = Date.now();
 	}
