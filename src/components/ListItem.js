51d6dd834b87d8577b1d0095a3a24ca1a80d082b
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-08-01 00:45:12 -0400

    Properly call click Handler

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,7 +6,8 @@ import {Download} from "react-feather";
 const ListItem = (props) => {
 	return (
 		<StyledItem>
-		{props.actionable ? <ItemButton onClick = {props.click(props.item, props.lastAction, props.cooldown)}><Download/></ItemButton> : null}
+		{console.log(props.item)}
+		{props.actionable ? <ItemButton onClick = {() => props.click(props.item, props.lastAction, props.cooldown)}><Download/></ItemButton> : null}
 		<p>{props.item}</p>
 		</StyledItem>
 	)
