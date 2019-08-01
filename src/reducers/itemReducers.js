44044f00c9de3dc7b23611f33e9f73e50c6db4a1
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 20:12:34 -0400

    Handle item Actions

---
+++ src/reducers/itemReducers.js
@@ -0,0 +1,24 @@
+import * as actions from "../actions";
+
+const initialState = {
+	"name": "",
+	"cooldown": 2.0,
+	"encumbrance": 2,  // How much are you carrying?
+	"strength": 10,  // How much can you carry?
+	"speed": 10,  // How fast do you travel?
+	"gold": 0,
+	"inventory": [],
+	"status": [],
+	"errors": [],
+	"messages": []
+}
+
+const branchTable = {
+	[actions.REFRESH_INVENTORY_STATUS]: (state, action) => {return {...action.payload}},
+	[actions.GET_ITEM]: (state, action) => {return {...state, inventory: [...state.inventory, action.payload]}},
+	[actions.DROP_ITEM]: (state, action) => {return {...state, inventory: state.inventory.filter(item => item !== action.payload)}}
+}
+
+export default (state = initialState, action) => {
+	return action.type in branchTable ? branchTable[action.type](state, action) : state;
+}
