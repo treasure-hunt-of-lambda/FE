190cbd738cd050effebf165a96831ba064060ef1
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-31 21:39:51 -0400

    Create Inventory file

---
+++ src/components/Inventory.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+
+const Inventory = (props) => {
+	return (
+		<InventoryWrapper>
+
+		</InventoryWrapper>
+	)
+}
+
+export default Inventory;
+
+const InventoryWrapper = styled.div`
+
+`;
